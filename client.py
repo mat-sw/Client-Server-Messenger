03446be4c9025eed13b8309316a9b211e5d3f439
juliatoklowicz <julsonem@gmail.com>
2022-01-16 19:28:14 +0100

    finished registration and login code

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import os
 import socket
 import threading
 import tkinter
@@ -38,22 +39,117 @@ class Client:
 
         self.register_label = tkinter.Label(self.register_screen, text="Please enter details below", bg="blue").pack(padx=20, pady=5)
 
-        username = tkinter.StringVar(self.register_screen)
-        password = tkinter.StringVar(self.register_screen)
+        self.username = tkinter.StringVar(self.register_screen)
+        self.password = tkinter.StringVar(self.register_screen)
 
         self.username_label = tkinter.Label(self.register_screen, text="Username * ")
         self.username_label.pack(padx=20, pady=5)
 
-        self.username_entry = tkinter.Entry(self.register_screen, textvariable=username)
+        self.username_entry = tkinter.Entry(self.register_screen, textvariable=self.username)
         self.username_entry.pack(padx=20, pady=5)
 
         self.password_label = tkinter.Label(self.register_screen, text="Password * ")
         self.password_label.pack(padx=20, pady=5)
 
-        self.password_entry = tkinter.Entry(self.register_screen, textvariable=password, show='*')
+        self.password_entry = tkinter.Entry(self.register_screen, textvariable=self.password, show='*')
         self.password_entry.pack(padx=20, pady=5)
 
-        self.reg_button = tkinter.Button(self.register_screen, text="Register", width=10, height=1, bg="blue").pack(padx=20, pady=5)
+        self.reg_button = tkinter.Button(self.register_screen, text="Register", width=10, height=1, bg="blue", command = self.register_user).pack(padx=20, pady=5)
+
+
+    def register_user(self):
+        username_info = self.username.get()
+        password_info = self.password.get()
+
+        file = open(username_info, "w")
+
+        file.write(username_info + "\n")
+        file.write(password_info)
+        file.close()
+
+        self.username_entry.delete(0,END)
+        self.password_entry.delete(0,END)
+
+        Label(self.register_screen, text = "Registration Success", fg = "green", font=("calibri", 11)).pack()
+
+
+    def delete_login_success(self):
+        self.login_sucess_screen.destroy()
+
+    def login_sucess(self):
+        self.login_sucess_screen = tkinter.Toplevel(self.login_screen)
+        self.login_sucess_screen.title("Success")
+        self.login_sucess_screen.geometry("150x100")
+        Label(self.login_sucess_screen, text = "Login success").pack()
+
+        Button(self.login_sucess_screen, text = "OK", command = self.delete_login_success).pack()
+
+
+    def delete_password_not_recognised(self):
+        self.password_not_recog_screen.destroy()
+
+
+    def password_not_recognised(self):
+        self.password_not_recog_screen = tkinter.Toplevel(self.login_screen)
+        self.password_not_recog_screen.title("Unsuccess")
+        self.password_not_recog_screen.geometry("150x100")
+
+        Label(self.password_not_recog_screen, text = "Invalid password").pack()
+        Button(self.password_not_recog_screen, text = "OK", command = self.delete_password_not_recognised).pack()
+
+    def delete_user_not_found_screen(self):
+        self.user_not_found_screen.destroy()
+
+    def user_not_found(self):
+        self.user_not_found_screen = tkinter.Toplevel(self.login_screen)
+        self.user_not_found_screen.title("Unsuccess")
+        self.user_not_found_screen.geometry("150x100")
+
+        Label(self.user_not_found_screen, text = "User not found").pack()
+        Button(self.user_not_found_screen, text = "OK", command = self.delete_user_not_found_screen).pack()
+
+    def login_verification(self):
+        username1 = self.username_verify.get()
+        password1 = self.password_verify.get()
+
+        self.username_login_entry.delete(0, END)
+        self.password_login_entry.delete(0, END)
+
+        self.list_of_files = os.listdir()
+
+        if username1 in self.list_of_files:
+            file1 = open(username1, "r")
+
+            verify = file1.read().splitlines()
+            if password1 in verify:
+                self.login_sucess()
+            else:
+                self.password_not_recognised()
+
+        else:
+            self.user_not_found()
+
+
+    def login(self):
+        self.login_screen = tkinter.Toplevel(self.win)
+        self.login_screen.title("Login")
+        self.login_screen.geometry("200x200")
+
+        self.login_lab = tkinter.Label(self.login_screen, text="Please enter details below to login", bg="blue").pack(padx=20, pady=5)
+
+        self.username_verify = tkinter.StringVar(self.login_screen)
+        self.password_verify = tkinter.StringVar(self.login_screen)
+
+        Label(self.login_screen, text = "Username * ").pack()
+        self.username_login_entry = tkinter.Entry(self.login_screen, textvariable = self.username_verify)
+        self.username_login_entry.pack()
+
+        Label(self.login_screen, text = "Password * ").pack()
+        self.password_login_entry = tkinter.Entry(self.login_screen, textvariable = self.password_verify, show = '*')
+        self.password_login_entry.pack()
+
+        Button(self.login_screen, text = "Login", width = 10, height = 1, command = self.login_verification).pack()
+
 
 
 
@@ -66,7 +162,7 @@ class Client:
         self.login_label = tkinter.Label(self.win, text="Login or register", bg="blue", width="300", height="2",
                                          font=("Calibri", 13)).pack(padx=20, pady=5)
 
-        self.login_button = tkinter.Button(self.win, text="Login", height="2", width="30").pack(padx=20, pady=5)
+        self.login_button = tkinter.Button(self.win, text="Login", height="2", width="30", command = self.login).pack(padx=20, pady=5)
 
         self.register_button = tkinter.Button(self.win, text="Register", height="2", width="30", command = self.register).pack(padx=20, pady=5)
 
